7b7a2a09cb53e99899f0a78da332f7d82d658e67
alfredan <alfredan@kth.se>
2016-11-18 10:40:56 +0100

    Setup libGDX project

---
+++ desktop/src/net/alfredandersson/indaplusplus/raytracer/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package net.alfredandersson.indaplusplus.raytracer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.alfredandersson.indaplusplus.raytracer.Main;
+
+public class DesktopLauncher {
+  
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    
+    new LwjglApplication(new Main(), config);
+  }
+}
