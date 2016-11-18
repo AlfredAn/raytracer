6e7df6bcf635e3c89c496424382bfce25785676a
alfredan <alfredan@kth.se>
2016-11-18 17:37:28 +0100

    Now using a different method of ray/sphere intersection This allows the intersection point and normals to be computed as well

--- core/src/net/alfredandersson/indaplusplus/raytracer/Sphere.java
+++ core/src/net/alfredandersson/indaplusplus/raytracer/Sphere.java
@@ -2,33 +2,50 @@ package net.alfredandersson.indaplusplus.raytracer;
 
 public final class Sphere implements Shape {
   
-  public final float x, y, z, radius;
+  public final float x, y, z, radius, invradius;
   
   public Sphere(float x, float y, float z, float radius) {
     this.x = x;
     this.y = y;
     this.z = z;
     this.radius = radius;
+    this.invradius = 1.0f / radius;
   }
   
   @Override
   public void raycast(RaycastResult result, float xStart, float yStart, float zStart, float xDir, float yDir, float zDir) {
-    // project (start -> (x, y, z)) onto (dir)
     float dx = x - xStart;
     float dy = y - yStart;
     float dz = z - zStart;
     
-    // dir is assumed to be normalized
-    float projLength = dx * xDir + dy * yDir + dz * zDir;
+    // quadratic formula
+    float a = xDir * xDir + yDir * yDir + zDir * zDir;
+    float b = -2 * (xDir * dx + yDir * dy + zDir * dz);
+    float c = dx * dx + dy * dy + dz * dz - radius * radius;
     
-    float startDistSqr = dx * dx + dy * dy + dz * dz;
-    float minDist = (float)Math.sqrt(startDistSqr - projLength * projLength);
+    float insideSqrt = b * b - 4 * a * c;
     
-    if (minDist >= 0 && minDist <= radius) {
-      // TODO: find point of intersection
-      result.set(this, projLength, 0, 0, 0, 0, 0, 0);
-    } else {
+    if (insideSqrt < 0) {
       result.setNoCollision();
+      return;
     }
+    
+    float t = (-b - (float)Math.sqrt(insideSqrt)) * 0.5f / a;
+    //float t2 = (-b + (float)Math.sqrt(insideSqrt)) * 0.5f / a; // this will only be useful if the camera is inside the sphere --- ignore it for performance
+    
+    if (t < 0) {
+      result.setNoCollision();
+      return;
+    }
+    
+    float hitX = xStart + xDir * t;
+    float hitY = yStart + yDir * t;
+    float hitZ = zStart + zDir * t;
+    
+    float normX = (hitX - x) * invradius;
+    float normY = (hitY - y) * invradius;
+    float normZ = (hitZ - z) * invradius;
+    
+    result.set(this, t, hitX, hitY, hitZ, normX, normY, normZ);
   }
 }
